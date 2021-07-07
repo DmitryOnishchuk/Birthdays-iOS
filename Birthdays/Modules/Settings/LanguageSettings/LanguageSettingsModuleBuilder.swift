01ae6ca4789b6bbd3e05459001cc9b29b37d7c31
Dmitry Onishchuk <dmitry.oni90@gmail.com>
2021-07-07 15:54:39 +0300

    Change language improve

---
+++ Birthdays/Modules/Settings/LanguageSettings/LanguageSettingsModuleBuilder.swift
@@ -0,0 +1,18 @@
+//
+//  LanguageSettingsModuleBuilder.swift
+//  Birthdays
+//
+//  Created by Dmitry Onishchuk on 01.07.2021.
+//  Copyright © 2021 Dmitry Onishchuk. All rights reserved.
+//
+
+import UIKit
+
+struct LanguageSettingsModuleBuilder {
+    
+    func create() -> UITableViewController {
+        let languageSettings = UIStoryboard(name: "LanguageSettings", bundle: nil).instantiateViewController(withIdentifier: "LanguageSettingsTableVC") as! LanguageSettingsTableVC
+        return languageSettings
+    }
+    
+}
---
+++ Birthdays/Modules/Settings/NotificationSettings/NotificationSettingsModuleBuilder.swift
@@ -0,0 +1,18 @@
+//
+//  NotificationSettingsModuleBuilder.swift
+//  Birthdays
+//
+//  Created by Dmitry Onishchuk on 01.07.2021.
+//  Copyright © 2021 Dmitry Onishchuk. All rights reserved.
+//
+
+import UIKit
+
+struct NotificationSettingsModuleBuilder {
+    
+    func create() -> UITableViewController {
+        let notificationSettings = UIStoryboard(name: "NotificationSettings", bundle: nil).instantiateViewController(withIdentifier: "NotificationSettingsTableVC") as! NotificationSettingsTableVC
+        return notificationSettings
+    }
+    
+}
