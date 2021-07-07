2c213910c1b703870def098af471e7587e9fc080
Dmitry Onishchuk <dmitry.oni90@gmail.com>
2021-07-07 16:22:48 +0300

    Colors Added

---
+++ Birthdays/Common/Colors.swift
@@ -0,0 +1,13 @@
+//
+//  Colors.swift
+//  Birthdays
+//
+//  Created by Dmitry Onishchuk on 07.07.2021.
+//  Copyright © 2021 Dmitry Onishchuk. All rights reserved.
+//
+
+import UIKit
+
+struct Colors {
+    static let orangeMain = UIColor(named: "OrangeMain")
+}
